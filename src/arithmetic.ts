a27866cedafe7fcd0707c5a85642f6cbf5e79075
rajasingh545 <rajasingh545@gmail.com>
2024-01-05 22:39:15 +0530

    Initial commit

---
+++ src/arithmetic.ts
@@ -0,0 +1,13 @@
+// Simple Arithmetic in TypeScript
+
+// Addition
+export const add = (num1: number, num2: number): number => num1 + num2;
+
+// Subtraction
+export const subtract = (num1: number, num2: number): number => num1 - num2;
+
+// Division
+export const divide = (num1: number, num2: number): number => num1 / num2;
+
+// Multiplication
+export const multiply = (num1: number, num2: number): number => num1 * num2;
